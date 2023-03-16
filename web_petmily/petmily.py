56abf1588a190217b351db08afab93a47da50731
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-03-17 00:42:32 +0900

    up25

--- web_petmily/petmily.py
+++ web_petmily/petmily.py
@@ -120,9 +120,8 @@ col1,col2 = st.columns([1,1])
 
 with col1 :
   # column 1 에 담을 내용
-  st.markdown("**:blue[동물 병원] 이용 순위**")  
-  st.altair_chart(a, theme="streamlit", use_container_width=True)
-
+  st.markdown("**:blue[동물 병원] 이용 순위**")
+  st.bar_chart(sph_sort[['소재지전체주소', '사업장명']])
   st.image("https://user-images.githubusercontent.com/71927533/225588437-0a7d6c29-27fa-48d9-b652-54573a4e35b6.png")
   st.info('자치구별 동물 병원수 입니다.', icon="ℹ️")
   
