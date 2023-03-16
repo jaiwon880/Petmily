7639d1a73c91cb14ec2f31725e06d96c70321ddf
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-03-16 16:02:15 +0900

    up2

--- web_petmily/app.py
+++ web_petmily/app.py
@@ -5,7 +5,7 @@ import pandas as pd
 import matplotlib.pyplot as plt
 matplotlib.use('Agg')       #서버에서, 화면에 표시하기 위해서 필요
 import seaborn as sns
-import altair as alt               ##https://altair-viz.github.io/
+import altair as alt         
 import plotly.express as px
 petCon = pd.read_csv('../data/동물위탁관리업.csv')
 
