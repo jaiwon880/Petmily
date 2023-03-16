e2e187689a658df1acd219ba26f35b703d76b867
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-03-17 01:27:40 +0900

    d

--- web_petmily/petmily.py
+++ web_petmily/petmily.py
@@ -117,17 +117,17 @@ sph_sort3 = sph_sort2.sort_values(by=['사업장명'], ascending=False)
 # 알테어 바 차트 생성
 bar_chart = alt.Chart(sph_sort3).mark_bar(
 ).encode(
-    x=alt.X('소재지전체주소',axis=alt.Axis(title=False,labelFontSize=2.0,labelAngle=-45.0)),
-    y=alt.Y('사업장명',axis=alt.Axis(title=False),
+    x=alt.X('소재지전체주소',axis=alt.Axis(labelFontSize=2.0,labelAngle=-45.0)),
+    y=alt.Y('사업장명'),
     color=alt.Color('소재지전체주소', scale=alt.Scale(scheme='darkblue'), legend=None)
-)).properties(
+).properties(
 )
 
 # Streamlit 앱 구성
 
 
 
-col1,col2 = st.columns([1,1])
+col1,col2 = st.columns([2,2])
 # 공간을 2:3 으로 분할하여 col1과 col2라는 이름을 가진 컬럼을 생성
 
 with col1 :
