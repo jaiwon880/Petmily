dd764ec3af165f4751e5768489c29a2d4782573c
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-03-16 17:34:38 +0900

    이미지

--- app.py
+++ app.py
@@ -1,8 +1,13 @@
 # streamlit 라이브러리 호출
 import streamlit as st
+from PIL import Image
 # import graph.py
 
 st.title("팻밀리")
 
 st.subheader("반려견 행복지도")
+
+image = Image.open(image/'서울동물병원.jpg')
+
+st.image(image, caption='동물병원')
 
