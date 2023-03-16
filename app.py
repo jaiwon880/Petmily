04c33f4f9277975a1fb973ab722a86430ff1f9e5
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-03-16 17:58:53 +0900

    urltest

--- app.py
+++ app.py
@@ -1,11 +1,20 @@
-# streamlit 라이브러리 호출
-import streamlit as st
-from PIL import Image
-# import graph.py
+# # streamlit 라이브러리 호출
+# import streamlit as st
+# from PIL import Image
+# # import graph.py
 
-st.title("팻밀리")
-st.subheader("반려견 행복지도")
+# st.title("팻밀리")
+# st.subheader("반려견 행복지도")
 
-# image = Image.open(+'/PetHospital.png')
-# st.image(image, caption='동물병원')
-st.image(f"imagefiles/PetHospital.png", caption='동물병원')
+# # image = Image.open(+'/PetHospital.png')
+# # st.image(image, caption='동물병원')
+
+# st.image(f"imagefiles/PetHospital.png", caption='동물병원')
+
+
+
+import pandas as pd
+import numpy as np
+
+
+seoul_pet_hospital = pd.read_csv("https://raw.githubusercontent.com/whataLIN/sample_rp/main/data/PetHospital.csv")
