a9fa0a4a169578cb3aa3b53b49767d64f2d63f2a
whataLIN <102681611+whataLIN@users.noreply.github.com>
2023-03-16 15:34:37 +0900

    그려짐??

--- web_petmily/app.py
+++ web_petmily/app.py
@@ -1,12 +1,12 @@
 # streamlit 라이브러리 호출
 import streamlit as st
-import graph.py as pg
+import graph.py
 
 st.title("팻밀리")
 
 st.subheader("반려견 행복지도")
 
-st.write(pg.seoulPetHospital())
+st.write(graph.seoulPetHospital())
 
 
 #==========================================================
