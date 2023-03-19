5ea197d31c8fcacae8aaef08a06bd0cd4e342348
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-03-19 22:56:19 +0900

    dd

--- web_petmily/petmily.py
+++ web_petmily/petmily.py
@@ -90,7 +90,7 @@ sph_sort3 = sph_sort2.sort_values(by=['사업장명'], ascending=False)
 fig_hos = px.bar(sph_sort, x=sph_sort.index, y='사업장명', color='사업장명',
              color_continuous_scale='Blues',
              labels={'x': '자치구', 'y': '동물병원 수'},
-             height=400)
+             height=400, width=600)
 fig_hos.update_layout(
     title='서울시 자치구별 동물병원 수',
     showlegend=True,
