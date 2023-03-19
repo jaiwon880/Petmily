ea3bcb14168ac49d839057533ceed5577ae5887b
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-03-19 22:32:14 +0900

    update8

--- web_petmily/petmily.py
+++ web_petmily/petmily.py
@@ -379,6 +379,38 @@ fig_syn.update_layout(
 )
 
 
+
+result2 = result[['동물병원순위', '동물미용업체순위', '위탁 업체 수',"보유 비율 순위","공원순위"]]
+
+# 오각형 방사형 차트 생성 및 출력
+df = pd.DataFrame(result2, index=address)
+
+for i in range(len(address)):
+    gn = df.loc[address[i]]
+    # 오각형 방사형 차트 생성
+    fig_radar = px.line_polar(
+        r=gn.values,
+        theta=gn.index,
+        line_close=True,
+    )
+    fig_radar.update_traces(fill='toself')
+    # 차트 레이아웃 설정
+    fig_radar.update_layout(
+        polar=dict(
+            radialaxis=dict(visible=True, range=[0, 30]),
+            angularaxis=dict(direction="clockwise"),
+        ),
+        showlegend=False,
+        title=f"{address[i]} 종합 시각화 차트"
+    )
+    # 차트 출력
+    fig_radar.show()
+
+
+st.plotly_chart(fig_radar, theme="streamlit", use_container_width=True)
+
+
+
 col1,col2 = st.columns([1,1])
 # 공간을 2:3 으로 분할하여 col1과 col2라는 이름을 가진 컬럼을 생성
 
@@ -414,34 +446,3 @@ with col2 :
   
 # 데이터 출처 :
 
-
-
-result2 = result[['동물병원순위', '동물미용업체순위', '위탁 업체 수',"보유 비율 순위","공원순위"]]
-result2
-
-# 데이터 프레임 생성
-df = pd.DataFrame(result2, index=address)
-# 오각형 방사형 차트 생성 및 출력
-for i in range(len(address)):
-    gn = df.loc[address[i]]
-    # 오각형 방사형 차트 생성
-    fig_radar = px.line_polar(
-        r=gn.values,
-        theta=gn.index,
-        line_close=True,
-    )
-    fig_radar.update_traces(fill='toself')
-    # 차트 레이아웃 설정
-    fig_radar.update_layout(
-        polar=dict(
-            radialaxis=dict(visible=True, range=[0, 30]),
-            angularaxis=dict(direction="clockwise"),
-        ),
-        showlegend=False,
-        title=f"{address[i]} 종합 시각화 차트"
-    )
-    # 차트 출력
-    fig_radar.show()
-
-
-st.plotly_chart(fig_radar, theme="streamlit", use_container_width=True)
