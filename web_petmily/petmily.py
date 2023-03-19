bf83154082157880ff96114c3e9868356d258ddb
Han Marie <71927533+jaiwon880@users.noreply.github.com>
2023-03-19 23:01:25 +0900

    update16

--- web_petmily/petmily.py
+++ web_petmily/petmily.py
@@ -98,7 +98,7 @@ fig_hos.update_layout(
     yaxis_title='동물병원 수',
     font=dict(size=3),
 )
-fig_hos.update_xaxes(tickfont_size=10, ticks="outside")
+fig_hos.update_xaxes(tickfont_size=9)
 
 
 # # 동물 병원 수 Altair 차트 
